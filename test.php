ef84d1419d3459a5c00ca1cd848674b952159bb0
245680 <405181621@qq.com>
2014-12-01 16:29:20 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,15 @@
 this is Suzuki
 <?php
-phpinfo();
+$data = ['data' => 'this', 'data2' => 'that'];
+$data = http_build_query($data);
+$context = [
+  'http' => [
+    'method' => 'GET',
+    'header' => "custom-header: custom-value\r\n" .
+                "custom-header-two: custom-value-2\r\n",
+    'content' => $data
+  ]
+];
+$context = stream_context_create($context);
+$result = file_get_contents('http://1.pt100t.sinaapp.com/pushdemo.php', false, $context);
 ?>
